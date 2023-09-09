3d55ac5e14aff838cf5a0911da74ea255badc428
PhamDuy <vanduy342001@gmail.com>
2023-09-10 04:55:23 +0700

    Start App Chat

--- jatte/settings.py
+++ jatte/settings.py
@@ -33,6 +33,12 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/chat-admin/'
 LOGOUT_REDIRECT_URL = '/'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channel.layer.InMemoryChannelLayer',
+    }
+}
+
 # Application definition
 
 INSTALLED_APPS = [
